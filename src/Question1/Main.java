1a2a64cca5bcecb984df1b37d5399edc9a7f6580
merduran <m.erduran96@gmail.com>
2022-02-03 16:12:26 +0300

    Added new solution for first question.

--- src/Question1/Main.java
+++ src/Question1/Main.java
@@ -1,51 +1,57 @@
 package Question1;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.math.BigInteger;
+import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        Map<Integer,Integer> resultMap = new HashMap<Integer,Integer>();
+        Map<Integer, Map<Integer,BigInteger>> resultMap = new HashMap<Integer, Map<Integer,BigInteger>>();
 
         for(int number = 10; number<100; number++){
-            resultMap.put(number,reachPalindrome(number,0));
+            resultMap.put(number,reachPalindrome(BigInteger.valueOf(number),0,new HashMap<Integer,BigInteger>()));
         }
         for(Integer key : resultMap.keySet()){
-            System.out.println(key + " sayısı " + resultMap.get(key) + ". denemede palindrome olmuştur." );
+            System.out.println(key + " sayısı " + resultMap.get(key).size() + ". denemede palindrome olmuştur." );
+            System.out.println("Chain:");
+            for(Integer key2 : resultMap.get(key).keySet())
+            System.out.println( "Iteration: "+ key2 + " Result: "+resultMap.get(key).get(key2));
         }
+
+
     }
 
-    public static int reachPalindrome(int number, int counter){
+    public static Map<Integer,BigInteger> reachPalindrome(BigInteger number, int counter, Map<Integer,BigInteger> chainMap){
 
         boolean flag = false;
-        number += getNumberOfReverse(number);
-        flag = isPalindrome(number);
+        number = number.add(getNumberOfReverse(number));
         counter ++;
+        chainMap.put(counter,number);
+        flag = isPalindrome(number);
+
         if(flag){
-            return counter;
+            return chainMap;
         }
         else{
-            return reachPalindrome(number,counter);
+            return reachPalindrome(number,counter,chainMap);
         }
     }
 
-    public static boolean isPalindrome(int number){
-        if(number == getNumberOfReverse(number)){
+    public static boolean isPalindrome(BigInteger number){
+        if(number.equals(getNumberOfReverse(number))){
             return true;
         }
         else
             return false;
     }
 
-    public static int getNumberOfReverse(int number){
-        int reverseNumber = 0;
-        while(number != 0)
+    public static BigInteger getNumberOfReverse(BigInteger number){
+        BigInteger reverseNumber = BigInteger.valueOf(0);
+        while(!number.equals(BigInteger.valueOf(0)))
         {
-            int remainder = number % 10;
-            reverseNumber = reverseNumber * 10 + remainder;
-            number = number/10;
+            BigInteger remainder = number.mod(BigInteger.valueOf(10));
+            reverseNumber = remainder.add(reverseNumber.multiply(BigInteger.valueOf(10)));
+            number = number.divide(BigInteger.valueOf(10));
         }
         return reverseNumber;
     }
